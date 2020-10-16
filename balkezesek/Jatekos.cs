6c3acad00673cfc190417af449b3f849d7e7cabd
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-10-16 12:58:53 +0200

    Kisebb nagyobb hibákkal készen van a program

---
+++ balkezesek/Jatekos.cs
@@ -0,0 +1,20 @@
+﻿namespace balkezesek
+{
+    internal class Jatekos
+    {
+        public string nev { get; private set; }
+        public string elsorepalyan { get; private set; }
+        public string utoljarapalyan { get; private set; }
+        public int suly { get; private set; }
+        public int magassag { get; private set; }
+
+        public Jatekos(string nev, string elsorepalyan, string utoljarapalyan, int suly, int magassag)
+        {
+            this.nev = nev;
+            this.elsorepalyan = elsorepalyan;
+            this.utoljarapalyan = utoljarapalyan;
+            this.suly = suly;
+            this.magassag = magassag;
+        }
+    }
+}
--- balkezesek/Program.cs
+++ balkezesek/Program.cs
@@ -3,13 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace balkezesek
 {
     class Program
     {
+        static List<Jatekos> Lista = new List<Jatekos>();
+        static void Beolvas()
+        {
+            StreamReader olvas = new StreamReader("balkezesek.csv");
+            string[] jellemzok = olvas.ReadLine().Split(';');
+            while (!olvas.EndOfStream)
+            {
+                string[] seged = olvas.ReadLine().Split(';');
+                Lista.Add(new Jatekos(seged[0], seged[1], seged[2], int.Parse(seged[3]), int.Parse(seged[4])));
+            }
+            olvas.Close();
+        }
+
+        static void HarmadikFeladat()
+        {
+            Console.WriteLine($"3. feladat: {Lista.Count}");
+        }
+
+        static void NegyedikFeladat()
+        {
+            var oktoberi = from l in Lista
+                           where l.utoljarapalyan.Contains("1999-10")
+                           select new { magassag = l.magassag, nev = l.nev };
+            Console.WriteLine("4. feladat:");
+            foreach (var o in oktoberi)
+            {
+                Console.WriteLine($"\t {o.nev} {(o.magassag*2.54):N1} cm");
+            }
+        }
+
+        static void OtHatFeladat()
+        {
+            bool nemjo = true;
+            Console.WriteLine();
+            Console.Write("5. feladat: kérek egy 1990 és 1999 közötti számot!:");
+            int megadott5 = int.Parse(Console.ReadLine());
+            while (nemjo)
+            {
+                if (megadott5 > 1990 || megadott5 < 1999)
+                {
+                    nemjo = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Hibás adat, kérek egy 1990 és 1999 közötti számot!: ");
+                }
+            }
+
+            double atlag = 0;
+            int db = 0;
+            foreach (var l in Lista)
+            {
+                string[] seged1 = l.elsorepalyan.Split('-');
+                string[] seged2 = l.utoljarapalyan.Split('-');
+                if (int.Parse(seged1[0]) <= megadott5 && megadott5 <= int.Parse(seged2[0]))
+                {
+                    atlag += l.suly;
+                    db++;
+                }
+            }
+            Console.WriteLine($"6. Feladat: {(atlag/db):N2} font");
+        }
+
+        
         static void Main(string[] args)
         {
+            Beolvas();
+            HarmadikFeladat();
+            NegyedikFeladat();
+            OtHatFeladat();
+            Console.ReadLine();
         }
     }
 }
